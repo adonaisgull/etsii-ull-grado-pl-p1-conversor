25abfab3786cdaae07cefc87d072f9ed28d5755e
SUAREZ GONZALEZ <alu4308@cc143.(none)>
2013-02-06 09:32:19 +0000

    Falta revisar la expresion regular

---
+++ README.txt
@@ -0,0 +1,6 @@
+Universidad de La Laguna
+Escuela Técnica Superior de Ingeniería Informática
+Grado en Ingeniería Informática
+Procesadores de Lenguajes
+
+Practica 1 - Conversor de temperaturas javascript
---
+++ temperature.js
@@ -0,0 +1,32 @@
+"use strict"
+
+function calculate() {
+
+	var result;
+	var original = document.getElementById("original");
+	var temp = original.value;
+	var regexp = /-?[0-9]*\.?[0-9]*[a-zA-Z]+/;
+
+	var m = temp.match(regexp);
+
+	if (m) {
+		var num = m.substring(m.length-1, 0);
+		var type = m.substring(1, m.length-1);
+		num = parseFloat(num);
+
+		if(type == 'c' || type == 'C') {
+			result = (num * 9 / 5) + 32;
+			result += 'C';
+		}
+		else {
+			result = (num - 32) * 5 / 9;
+			result += 'F';
+		}
+
+		converted.innerHTML = result;
+
+	}
+	else {
+		converted.innerHTML = "ERROR! Try something lik '-4.2' instead";
+	}
+}
